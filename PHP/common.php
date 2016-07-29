60119db8440b912f463cd6ffed70ff60a03d8344
Akashi_SN <btorntireinvynriy@gmail.com>
2016-07-30 00:10:31 +0900

    Update

--- PHP/common.php
+++ PHP/common.php
@@ -23,7 +23,7 @@
 
   function userid_get($u){
     $a = explode(";",$u);
-	  $b = "";
+    $b = "";
     for($i = 0;$i< count($a);$i++){
       $pos = strpos($a[$i],'userId');
       if ($pos !== false) {
@@ -102,76 +102,76 @@
   }
 
   function Score_to_rank($score){
-	  if($score >= 1007500){
-		  return "sss";
-	  }
-	  else if($score >= 1000000){
-		  return "ss";
-	  }
-	  else if($score >= 975000){
-		  return "s";
-	  }
-	  else if($score >= 950000){
-		  return "aaa";
-	  }
-	  else if($score >= 925000){
-		  return "aa";
-	  }
-	  else if($score >= 900000){
-		  return "a";
-	  }
-	  else if($score >= 800000){
-		  return "bbb";
-	  }
-	  else if($score >= 700000){
-		  return "bb";
-	  }
-	  else if($score >= 600000){
-		  return "b";
-	  }
-	  else if($score >= 500000){
-		  return "c";
-	  }
-	  else{
-		  return null;
-	  }
+    if($score >= 1007500){
+      return "sss";
+    }
+    else if($score >= 1000000){
+      return "ss";
+    }
+    else if($score >= 975000){
+      return "s";
+    }
+    else if($score >= 950000){
+      return "aaa";
+    }
+    else if($score >= 925000){
+      return "aa";
+    }
+    else if($score >= 900000){
+      return "a";
+    }
+    else if($score >= 800000){
+      return "bbb";
+    }
+    else if($score >= 700000){
+      return "bb";
+    }
+    else if($score >= 600000){
+      return "b";
+    }
+    else if($score >= 500000){
+      return "c";
+    }
+    else{
+      return null;
+    }
   }
   function score_to_rate($score,$base_rate){
-	  if($score >= 1007500){
-		  return (double)($base_rate+2);
-	  }
-	  else if($score >= 1005000){
-		  return (double)($base_rate+1.5+($score-1005000)*10/50000);
-	  }
-	  else if($score >= 1000000){
-		  return (double)($base_rate+1+($score-1000000)*5/50000);
-	  }
-	  else if($score >= 975000){
-		  return (double)($base_rate+($score-975000)*2/50000);
-	  }
-	  else if($score >= 950000){
-		  return (double)($base_rate-1.5+($score-950000)*3/50000);
-	  }
-	  else if($score >= 925000){
-		  return (double)($base_rate-3+($score-925000)*3/50000);
-	  }
-	  else if($score >= 900000){
-		  return (double)($base_rate-5+($score-900000)*4/50000);
-	  }
-	  else if($score >= 800000){
-		  return (double)($base_rate-7.5+($score-800000)*1.25/50000);
-	  }
-	  else if($score >= 700000){
-		  return (double)($base_rate-8.5+($score-700000)*0.5/50000);
-	  }
-	  else if($score >= 600000){
-		  return (double)($base_rate-9+($score-600000)*0.25/50000);
-	  }
-	  else if($score >= 500000){
-		  return (double)($base_rate-13.7+($score-500000)*2.35/50000);
-	  }
-	  else{
-		  return null;
-	  }
+    if($score >= 1007500){
+      return (double)($base_rate+2);
+    }
+    else if($score >= 1005000){
+      return (double)($base_rate+1.5+($score-1005000)*10/50000);
+    }
+    else if($score >= 1000000){
+      return (double)($base_rate+1+($score-1000000)*5/50000);
+    }
+    else if($score >= 975000){
+      return (double)($base_rate+($score-975000)*2/50000);
+    }
+    else if($score >= 950000){
+      return (double)($base_rate-1.5+($score-950000)*3/50000);
+    }
+    else if($score >= 925000){
+      return (double)($base_rate-3+($score-925000)*3/50000);
+    }
+    else if($score >= 900000){
+      return (double)($base_rate-5+($score-900000)*4/50000);
+    }
+    else if($score >= 800000){
+      return (double)($base_rate-7.5+($score-800000)*1.25/50000);
+    }
+    else if($score >= 700000){
+      return (double)($base_rate-8.5+($score-700000)*0.5/50000);
+    }
+    else if($score >= 600000){
+      return (double)($base_rate-9+($score-600000)*0.25/50000);
+    }
+    else if($score >= 500000){
+      return (double)($base_rate-13.7+($score-500000)*2.35/50000);
+    }
+    else{
+      return null;
+    }
   }
 ?>
--- PHP/lib/chunithm.js
+++ PHP/lib/chunithm.js
@@ -49,9 +49,9 @@ ga('send', 'pageview');
 // グローバル変数
 //--------------------------------------------------------------------
 
-	var UserData;
-	var Best = "";
-	var Recent ="";
+  var UserData;
+  var Best = "";
+  var Recent ="";
 
 //--------------------------------------------------------------------
 // レート値取得
@@ -85,170 +85,170 @@ function JsonPost(Userid) {
     },
     // 成功・失敗に関わらず通信が終了した際の処理
     complete: function() {
-    	$("#overlay").fadeOut();
+      $("#overlay").fadeOut();
     }
   });  
 }
 
 //ユーザーデータの表示
 function UserRateDisp(){
-	var UserRate = UserData["User"];
-	document.getElementById('best-max').textContent = "BEST枠平均: " + UserRate["BestRate"].toFixed(2) + "/最大レート: " + UserRate["MaxRate"].toFixed(2); 
-	document.getElementById('recent-disp').textContent = "RECENT枠平均: " + UserRate["RecentRate-1"].toFixed(2) + "/表示レート: " + UserRate["DispRate"].toFixed(2);
-	//tweetボタン
-	twttr.ready(function() {
-	  var rate = "BEST枠平均: " + UserRate["BestRate"].toFixed(2) + " 最大レート: " + UserRate["MaxRate"].toFixed(2) + "\n" + "RECENT枠平均: " + UserRate["RecentRate-1"].toFixed(2) + " 表示レート: " + UserRate["DispRate"].toFixed(2) + "\n";
-	  twttr.widgets.createShareButton(
-	    'https://akashisn.info/?article=4',
-	    location.href,
-	    document.getElementById('tweet'),
-	    {
-	      lang: 'ja',
-	      size: 'large',
-	      text:  rate,
-	      hashtags : 'CHUNITHMRateCalculator'
-	    }
-	  )
-	});
+  var UserRate = UserData["User"];
+  document.getElementById('best-max').textContent = "BEST枠平均: " + UserRate["BestRate"].toFixed(2) + "/最大レート: " + UserRate["MaxRate"].toFixed(2); 
+  document.getElementById('recent-disp').textContent = "RECENT枠平均: " + UserRate["RecentRate-1"].toFixed(2) + "/表示レート: " + UserRate["DispRate"].toFixed(2);
+  //tweetボタン
+  twttr.ready(function() {
+    var rate = "BEST枠平均: " + UserRate["BestRate"].toFixed(2) + " 最大レート: " + UserRate["MaxRate"].toFixed(2) + "\n" + "RECENT枠平均: " + UserRate["RecentRate-1"].toFixed(2) + " 表示レート: " + UserRate["DispRate"].toFixed(2) + "\n";
+    twttr.widgets.createShareButton(
+      'https://akashisn.info/?article=4',
+      location.href,
+      document.getElementById('tweet'),
+      {
+        lang: 'ja',
+        size: 'large',
+        text:  rate,
+        hashtags : 'CHUNITHMRateCalculator'
+      }
+    )
+  });
 }
 
 //best枠の表示
 function BestRateDisp(){
-	if(Best == ""){
-		var element = "";
-		Best = '\
-	<div class="frame01 w460">\
-	  <div class="frame01_inside w450">\
-	    <h2 style="margin-top:10px;" id="page_title">BEST枠</h2>\
-	    <hr class="line_dot_black w420">\
-	    <div class="box01 w420">\
-	      <div class="mt_10">\
-	        <div id="userPlaylog_result">';
-	      //Best枠の数だけ繰り返す
-	      for(var i = 0; i < UserData["Best"].length; i++){
-	        var MusicDeteil = UserData["Best"][i];
-	        var MusicName = MusicDeteil["MusicName"];
-	        var MusicImg = MusicDeteil["Images"];
-	        var BaseRate = MusicDeteil["BaseRate"];
-	        var Score = MusicDeteil["Score"];
-	        var Rank = MusicDeteil["Rank"];
-	        var BestRate = MusicDeteil["BestRate"];
-	        var level = MusicDeteil["level"];
-	        
-	        if(i == 30){
-	            element += '\
-	        </div>\
-	      </div>\
-	    </div>\
-	  </div>\
-	</div>\
-	<div class="frame01 w460">\
-	  <div class="frame01_inside w450">\
-	    <h2 style="margin-top:10px;" id="page_title">BEST枠外</h2>\
-	    <hr class="line_dot_black w420">\
-	    <div class="box01 w420">\
-	      <div class="mt_10">\
-	        <div id="userPlaylog_result">';
-	          }
-	        element += '\
-	          <div class="frame02 w400">\
-	            <div class="play_jacket_side">\
-	              <div class="play_jacket_area">\
-	                <div id="Jacket" class="play_jacket_img">\
-	                  <img src="https://chunithm-net.com/mobile/'+MusicImg+'"">\
-	                </div>\
-	              </div>\
-	            </div>\
-		          <div class="play_data_side01">\
-		            <div class="box02 play_track_block">\
-		              <div id="TrackLevel" class="play_track_result">\
-		                <img src="https://chunithm-net.com/mobile/common/images/icon_'+level+'.png">\
-		              </div>\
-		            </div>\
-		            <div class="box02 play_musicdata_block">\
-		              <div id="MusicTitle" class="play_musicdata_title">'+MusicName+'</div>\
-		              <div class="play_musicdata_score clearfix">\
-		                <div class="play_musicdata_score_text">譜面定数:<span id="Score">'+BaseRate+'</span></div><br>\
-		                <div class="play_musicdata_score_text">RATING:<span id="Score">'+BestRate+'</span></div><br>\
-		                <div class="play_musicdata_score_text">Score：<span id="Score">'+Score+'</span></div>\
-		                <img src="https://chunithm-net.com/mobile/common/images/icon_'+Rank+'.png">\
-		              </div>\
-		            </div>\
-		          </div>\
-		        </div>';	          
-	      }
-	      Best += element;
-	    }      
+  if(Best == ""){
+    var element = "";
+    Best = '\
+  <div class="frame01 w460">\
+    <div class="frame01_inside w450">\
+      <h2 style="margin-top:10px;" id="page_title">BEST枠</h2>\
+      <hr class="line_dot_black w420">\
+      <div class="box01 w420">\
+        <div class="mt_10">\
+          <div id="userPlaylog_result">';
+        //Best枠の数だけ繰り返す
+        for(var i = 0; i < UserData["Best"].length; i++){
+          var MusicDeteil = UserData["Best"][i];
+          var MusicName = MusicDeteil["MusicName"];
+          var MusicImg = MusicDeteil["Images"];
+          var BaseRate = MusicDeteil["BaseRate"];
+          var Score = MusicDeteil["Score"];
+          var Rank = MusicDeteil["Rank"];
+          var BestRate = MusicDeteil["BestRate"];
+          var level = MusicDeteil["level"];
+          
+          if(i == 30){
+              element += '\
+          </div>\
+        </div>\
+      </div>\
+    </div>\
+  </div>\
+  <div class="frame01 w460">\
+    <div class="frame01_inside w450">\
+      <h2 style="margin-top:10px;" id="page_title">BEST枠外</h2>\
+      <hr class="line_dot_black w420">\
+      <div class="box01 w420">\
+        <div class="mt_10">\
+          <div id="userPlaylog_result">';
+            }
+          element += '\
+            <div class="frame02 w400">\
+              <div class="play_jacket_side">\
+                <div class="play_jacket_area">\
+                  <div id="Jacket" class="play_jacket_img">\
+                    <img src="https://chunithm-net.com/mobile/'+MusicImg+'"">\
+                  </div>\
+                </div>\
+              </div>\
+              <div class="play_data_side01">\
+                <div class="box02 play_track_block">\
+                  <div id="TrackLevel" class="play_track_result">\
+                    <img src="https://chunithm-net.com/mobile/common/images/icon_'+level+'.png">\
+                  </div>\
+                </div>\
+                <div class="box02 play_musicdata_block">\
+                  <div id="MusicTitle" class="play_musicdata_title">'+MusicName+'</div>\
+                  <div class="play_musicdata_score clearfix">\
+                    <div class="play_musicdata_score_text">譜面定数:<span id="Score">'+BaseRate+'</span></div><br>\
+                    <div class="play_musicdata_score_text">RATING:<span id="Score">'+BestRate+'</span></div><br>\
+                    <div class="play_musicdata_score_text">Score：<span id="Score">'+Score+'</span></div>\
+                    <img src="https://chunithm-net.com/mobile/common/images/icon_'+Rank+'.png">\
+                  </div>\
+                </div>\
+              </div>\
+            </div>';            
+        }
+        Best += element;
+      }      
       var div = document.getElementById( "inner" );
       div.innerHTML = Best;
 }
 
 //Recent枠の表示
 function RecentRateDisp(){
-	if(Recent == ""){
-		var element = "";
-		Recent = '\
-	<div class="frame01 w460">\
-	  <div class="frame01_inside w450">\
-	    <h2 style="margin-top:10px;" id="page_title">RECENT枠</h2>\
-	    <hr class="line_dot_black w420">\
-	    <div class="box01 w420">\
-	      <div class="mt_10">\
-	        <div id="userPlaylog_result">';
-	      //Best枠の数だけ繰り返す
-	      for(var i = 0; i < UserData["Recent"].length; i++){
-	        var MusicDeteil = UserData["Recent"][i];
-	        var MusicName = MusicDeteil["MusicName"];
-	        var MusicImg = MusicDeteil["Images"];
-	        var BaseRate = MusicDeteil["BaseRate"];
-	        var Score = MusicDeteil["Score"];
-	        var Rank = MusicDeteil["Rank"];
-	        var BestRate = MusicDeteil["BestRate"];
-	        var level = MusicDeteil["level"];
-	        if(i == 10){
-	            element += '\
-	        </div>\
-	      </div>\
-	    </div>\
-	  </div>\
-	</div>\
-	<div class="frame01 w460">\
-	  <div class="frame01_inside w450">\
-	    <h2 style="margin-top:10px;" id="page_title">RECENT枠外</h2>\
-	    <hr class="line_dot_black w420">\
-	    <div class="box01 w420">\
-	      <div class="mt_10">\
-	        <div id="userPlaylog_result">';
-	          }
-		        element += '\
-		          <div class="frame02 w400">\
-		            <div class="play_jacket_side">\
-		              <div class="play_jacket_area">\
-		                <div id="Jacket" class="play_jacket_img">\
-		                  <img src="https://chunithm-net.com/mobile/'+MusicImg+'"">\
-		                </div>\
-		              </div>\
-		            </div>\
-			          <div class="play_data_side01">\
-			            <div class="box02 play_track_block">\
-			              <div id="TrackLevel" class="play_track_result">\
-			                <img src="https://chunithm-net.com/mobile/common/images/icon_'+level+'.png">\
-			              </div>\
-			            </div>\
-			            <div class="box02 play_musicdata_block">\
-			              <div id="MusicTitle" class="play_musicdata_title">'+MusicName+'</div>\
-			              <div class="play_musicdata_score clearfix">\
-			                <div class="play_musicdata_score_text">譜面定数:<span id="Score">'+BaseRate+'</span></div><br>\
-			                <div class="play_musicdata_score_text">RATING:<span id="Score">'+BestRate+'</span></div><br>\
-			                <div class="play_musicdata_score_text">Score：<span id="Score">'+Score+'</span></div>\
-			                <img src="https://chunithm-net.com/mobile/common/images/icon_'+Rank+'.png">\
-			              </div>\
-			            </div>\
-			          </div>\
-			        </div>';			      
-	      }
-	      Recent += element;
-	    }      
+  if(Recent == ""){
+    var element = "";
+    Recent = '\
+  <div class="frame01 w460">\
+    <div class="frame01_inside w450">\
+      <h2 style="margin-top:10px;" id="page_title">RECENT枠</h2>\
+      <hr class="line_dot_black w420">\
+      <div class="box01 w420">\
+        <div class="mt_10">\
+          <div id="userPlaylog_result">';
+        //Best枠の数だけ繰り返す
+        for(var i = 0; i < UserData["Recent"].length; i++){
+          var MusicDeteil = UserData["Recent"][i];
+          var MusicName = MusicDeteil["MusicName"];
+          var MusicImg = MusicDeteil["Images"];
+          var BaseRate = MusicDeteil["BaseRate"];
+          var Score = MusicDeteil["Score"];
+          var Rank = MusicDeteil["Rank"];
+          var BestRate = MusicDeteil["BestRate"];
+          var level = MusicDeteil["level"];
+          if(i == 10){
+              element += '\
+          </div>\
+        </div>\
+      </div>\
+    </div>\
+  </div>\
+  <div class="frame01 w460">\
+    <div class="frame01_inside w450">\
+      <h2 style="margin-top:10px;" id="page_title">RECENT枠外</h2>\
+      <hr class="line_dot_black w420">\
+      <div class="box01 w420">\
+        <div class="mt_10">\
+          <div id="userPlaylog_result">';
+            }
+            element += '\
+              <div class="frame02 w400">\
+                <div class="play_jacket_side">\
+                  <div class="play_jacket_area">\
+                    <div id="Jacket" class="play_jacket_img">\
+                      <img src="https://chunithm-net.com/mobile/'+MusicImg+'"">\
+                    </div>\
+                  </div>\
+                </div>\
+                <div class="play_data_side01">\
+                  <div class="box02 play_track_block">\
+                    <div id="TrackLevel" class="play_track_result">\
+                      <img src="https://chunithm-net.com/mobile/common/images/icon_'+level+'.png">\
+                    </div>\
+                  </div>\
+                  <div class="box02 play_musicdata_block">\
+                    <div id="MusicTitle" class="play_musicdata_title">'+MusicName+'</div>\
+                    <div class="play_musicdata_score clearfix">\
+                      <div class="play_musicdata_score_text">譜面定数:<span id="Score">'+BaseRate+'</span></div><br>\
+                      <div class="play_musicdata_score_text">RATING:<span id="Score">'+BestRate+'</span></div><br>\
+                      <div class="play_musicdata_score_text">Score：<span id="Score">'+Score+'</span></div>\
+                      <img src="https://chunithm-net.com/mobile/common/images/icon_'+Rank+'.png">\
+                    </div>\
+                  </div>\
+                </div>\
+              </div>';            
+        }
+        Recent += element;
+      }      
       var div = document.getElementById( "inner" );
       div.innerHTML = Recent;
 }
