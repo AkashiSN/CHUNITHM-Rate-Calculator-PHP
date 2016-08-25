dbed36668aa3207952870e36528dcbda5bb33487
AkashiSN <btorntireinvynriy@gmail.com>
2016-08-26 00:10:54 +0900

    Update

--- PHP/chunithm.php
+++ PHP/chunithm.php
@@ -67,10 +67,10 @@ if(!isset($_GET['user'])){
   <title>CHUNITHM Rate Calculator</title>
   <link rel="stylesheet" href="https://chunithm-net.com/mobile/common/css/common.css" />
   <link rel="stylesheet" href="https://chunithm-net.com/mobile/common/css/contents.css" />
-  <link rel="stylesheet" href="lib/chunithm.css?var=3.1.0" />
+  <link rel="stylesheet" href="lib/chunithm.css?var=3.0.1" />
   <script src="/common/js/jquery-1.12.4.min.js" ></script>
   <script src="https://platform.twitter.com/widgets.js"></script>
-  <script src="lib/chunithm.js?var=3.1.0" ></script>
+  <script src="lib/chunithm.js?var=3.0.2" ></script>
   <script type="text/javascript">
     // DOMを全て読み込んだあとに実行される
     $(function() {
